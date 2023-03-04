43a9cbb35c603142cb211b98900d1f40996fba30
Zheng20010712 <Zheng20010712@163.com>
2023-03-04 10:30:50 +0800

    feat: 实现mini-react-render

--- mini-react-render/src/customRenderer.js
+++ mini-react-render/src/customRenderer.js
@@ -5,28 +5,101 @@
  * @email: zheng20010712@163.com
  * @Date: 2023-03-03 21:41:16
  * @LastEditors: ZhengXiaoRui
- * @LastEditTime: 2023-03-03 22:50:48
+ * @LastEditTime: 2023-03-04 10:28:14
  */
 import ReactReconciler from "react-reconciler";
 
 const hostConfig = {
+  //宿主环境的API是否支持Mutation
   supportsMutation: true,
-  getRootHostContext() {},
-  getChildHostContext() {},
-  prepareForCommit() {},
+
+  //初始上下文信息
+  getRootHostContext() {
+    return {};
+  },
+  getChildHostContext() {
+    return {};
+  },
+
+  prepareForCommit() {
+    return true;
+  },
   resetAfterCommit() {},
-  shouldSetTextContent() {},
-  createInstance() {},
-  createTextInstance() {},
-  appendInitialChild() {},
+
+  //组件的Children是否为文本节点
+  shouldSetTextContent(_, props) {
+    return (
+      typeof props.children === "string" || typeof props.children === "number"
+    );
+  },
+
+  //创建DOM元素
+  createInstance(
+    type,
+    newProps,
+    rootContainerInstance,
+    _currentHostContext,
+    workInProgress
+  ) {
+    const domElement = document.createElement(type);
+    Object.keys(newProps).forEach((propName) => {
+      const propValue = newProps[propName];
+      if (propName === "children") {
+        if (typeof propValue === "string" || typeof propName === "number") {
+          domElement.textContent = propValue;
+        }
+      } else if (propName === "className") {
+        domElement.setAttribute("class", propValue);
+      } else if (propName === "onClick") {
+        domElement.addEventListener("click", propValue);
+      } else {
+        domElement.setAttribute(propName, propValue);
+      }
+    });
+    return domElement;
+  },
+  createTextInstance(text) {
+    return document.createTextNode(text);
+  },
+
+  //插入DOM元素——对应placement flag
+  appendInitialChild(parent, child) {
+    parent.appendChild(child);
+  },
+
+  //设置DOM元素属性
   finalizeInitialChildren() {},
+
   clearContainer() {},
-  appendChild() {},
-  appendChildToContainer() {},
-  prepareUpdate() {},
-  commitUpdate() {},
-  commitTextUpdate() {},
-  removeChild() {},
+  appendChild(parent, child) {
+    parent.appendChild(child);
+  },
+  appendChildToContainer(parent, child) {
+    parent.appendChild(child);
+  },
+  prepareUpdate(domElement, oldProps, newProps) {
+    return true;
+  },
+  commitUpdate(domElement, updatePayload, type, oldProps, newProps) {
+    Object.keys(newProps).forEach((propName) => {
+      const propValue = newProps[propName];
+      if (propName === "children") {
+        if (typeof propValue === "string" || typeof propValue === "number") {
+          domElement.textContent = propValue;
+        }
+      } else {
+        domElement.setAttribute(propName, propValue);
+      }
+    });
+  },
+  commitTextUpdate(textInstance, oldText, newText) {
+    textInstance.text = newText;
+  },
+
+  //删除子DOM元素——对应ChildDeletion flag
+  removeChild(parentInstance, child) {
+    parentInstance.removeChild(child);
+  },
 };
 
 const ReactReconcilerInst = ReactReconciler(hostConfig);
--- mini-react-render/src/index.js
+++ mini-react-render/src/index.js
@@ -5,7 +5,7 @@
  * @email: zheng20010712@163.com
  * @Date: 2023-03-03 21:06:05
  * @LastEditors: ZhengXiaoRui
- * @LastEditTime: 2023-03-03 22:32:49
+ * @LastEditTime: 2023-03-04 10:29:01
  */
 import React from "react";
 // import ReactDOM from "react-dom/client";
@@ -19,4 +19,4 @@ customRenderer.render(<App />, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
